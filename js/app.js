3c4b4f632c7019d152ad5c31b5b992d36732c0c4
Vitor Macedo <macedov836@gmai.com>
2025-04-23 11:09:57 -0300

    Adicionando validaçoes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,53 @@
-function adicionar() {
-let amigo = document.getElementById('nome-amigo');
-let lista = document.getElementById('lista-amigos');
+let amigos = [];
 
-    if (lista.textContent == '') {
-     lista.textContent = amigo.value;
-    } else {
-    lista.textContent = lista.textContent + ', ' + amigo.value;
+function adicionar() {
+
+    let amigo = document.getElementById('nome-amigo');
+    if (amigo.value == '' ) {
+        alert('Informe o nome do amigo!');
+        return;
     }
+
+    let lista = document.getElementById('lista-amigos');
+
+    if (amigo.value.trim() === '') return;
+
+    amigos.push(amigo.value);
+
+    if (lista.textContent === '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent += ', ' + amigo.value;
+    }
+
     amigo.value = '';
+}
+
+function sortear() {
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let proximo = (i + 1) % amigos.length;
+        sorteio.innerHTML += amigos[i] + ' --> ' + amigos[proximo] + '<br>';
+    }
+}
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
